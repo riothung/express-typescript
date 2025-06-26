b214402af1648acddd16a8ba93435a32738988bd
Rio Thung <mario.thung@gmail.com>
2025-06-26 09:16:40 +0700

    feat: enable parsing JSON body in request with express.json()

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import express from "express";
 const app = express();
 const PORT = 8080;
 
+app.use(express.json());
+
 app.listen(PORT, () => {
     console.log(`Server is listening on port: ${PORT}`);
 })
